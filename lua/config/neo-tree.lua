40aac67d6674c72195ca38e33db6311b03898433
Florian Walter <nairolf.retlaw@gmail.com>
2023-04-12 09:28:38 +0200

    add ability to open files using the system application in neotree

--- lua/config/neo-tree.lua
+++ lua/config/neo-tree.lua
@@ -3,8 +3,29 @@ if not status_ok then
   return
 end
 
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+local os = vim.loop.os_uname()
+
+local open_cmd = 'xdg-open'
+if os.sysname == 'Darwin' then
+  open_cmd = 'open'
+end
+
 neo_tree.setup({
+  close_if_last_window = true,
+  open_files_do_not_replace_types = { "terminal", "trouble" },
   window = {
     width = 50,
+  },
+  filesystem = {
+    window = {
+      mappings = {
+        ["go"] = function(state)
+          local node = state.tree:get_node()
+          io.popen("/usr/bin/env '" .. open_cmd .. "' '" .. node.path .. "'")
+        end,
+      },
+    },
   },
 })
