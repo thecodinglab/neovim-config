939e509ed0e1a55fee5e69edc9bc0a24edab1028
Florian Walter <nairolf.retlaw@gmail.com>
2023-04-17 08:06:28 +0200

    add ability to automatically install language servers

--- lua/config/lsp/mason.lua
+++ lua/config/lsp/mason.lua
@@ -4,3 +4,22 @@ if not status_ok then
 end
 
 mason.setup({})
+
+function ensure_installed_packages()
+  local Package = require('mason-core.package')
+  local registry = require('mason-registry')
+
+  local server_config = require('config.lsp.server')
+  for _, server in pairs(server_config.ensure_installed) do
+    local name, version = Package.Parse(server)
+    local pkg = registry.get_package(name)
+
+    if not pkg:is_installed() then
+      pkg:install({
+        version = version,
+      })
+    end
+  end
+end
+
+ensure_installed_packages()
--- lua/config/lsp/server.lua
+++ lua/config/lsp/server.lua
@@ -1,3 +1,4 @@
 return {
+  ensure_installed = { 'clangd', 'gopls', 'lua-language-server' },
   enabled = { 'clangd', 'gopls', 'lua_ls' },
 }
