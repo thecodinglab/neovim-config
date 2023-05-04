afd8d1695bbb55e747e5da0e4f752b60d58d2d24
Florian Walter <nairolf.retlaw@gmail.com>
2023-05-04 08:43:32 +0200

    add ability to copy to system clipboard

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -33,3 +33,7 @@ vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>',
 -- indentation & stay in visual mode
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
+
+-- clipboard
+vim.keymap.set('v', '<leader>y', '"+y', opts)
+vim.keymap.set('n', '<leader>p', '"+p', opts)
