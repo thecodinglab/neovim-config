7d02b503a789093c026d54092a3b5576b7c4a199
Florian Walter <nairolf.retlaw@gmail.com>
2023-06-08 17:58:45 +0200

    add automatic theme selection based on current os theme

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -3,10 +3,17 @@ if not status_ok then
   return
 end
 
+local theme = require('core.theme')
+
+local lualine_theme = 'onedark'
+if theme.is_light() then
+  lualine_theme = 'onelight'
+end
+
 lualine.setup({
   options = {
     icons_enabled = true,
-    theme = 'onedark',
+    theme = lualine_theme,
     disabled_filetypes = { 'neo-tree', 'Outline' },
   },
 })
--- lua/core/theme.lua
+++ lua/core/theme.lua
@@ -1,10 +1,51 @@
-local status_ok, rosepine = pcall(require, 'rose-pine')
-if not status_ok then
-  return
+local M = {
+  DARK = 'dark',
+  LIGHT = 'light',
+
+  theme = 'dark',
+}
+
+function M.is_dark()
+  return M.theme == M.DARK
 end
 
-rosepine.setup({
-  variant = 'main',
-})
+function M.is_light()
+  return M.theme == M.LIGHT
+end
 
-vim.cmd.colorscheme('rose-pine')
+local function determine_os_theme()
+  local paths = vim.api.nvim_list_runtime_paths()
+  for _, path in ipairs(paths) do
+    local script = path .. '/scripts/os_theme.sh'
+
+    local handle = io.popen(script)
+    if handle ~= nil then
+      local result = handle:read('*a')
+      handle:close()
+
+      if result ~= nil and result ~= 'unknown' then
+        return result:gsub('%s+', '')
+      end
+    end
+  end
+
+  return M.DARK
+end
+
+local function select(theme)
+  M.theme = theme
+
+  local variant = 'main'
+  if M.is_light() then
+    variant = 'dawn'
+  end
+
+  require('rose-pine').colorscheme(variant)
+end
+
+
+M.select = select
+
+select(determine_os_theme())
+
+return M
