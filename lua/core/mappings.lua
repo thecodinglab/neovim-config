40082383341a8ec252e66fed46fcb74cdf3f97dd
Florian Walter <nairolf.retlaw@gmail.com>
2024-01-09 12:58:57 +0100

    add keybinding to move lines around
    disable tmux resize keybindings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -5,7 +5,7 @@ local opts = { noremap = true, silent = true }
 -- disable vim keyword search
 vim.keymap.set('i', 'C-n', '<nop>', opts)
 
--- indentation & stay in visual mode
+-- indent while staying in visual mode
 vim.keymap.set('v', '<', '<gv', opts)
 vim.keymap.set('v', '>', '>gv', opts)
 
@@ -15,6 +15,10 @@ vim.keymap.set('n', '<leader>p', '"+p', opts)
 
 -- save
 vim.keymap.set('n', '<leader>w', '<cmd>silent write<cr>', opts)
+
+-- move lines
+vim.keymap.set('n', '<A-j>', '<cmd>m .+1<cr>', opts)
+vim.keymap.set('n', '<A-k>', '<cmd>m .-2<cr>', opts)
 
 -- create scratch buffer
 local function create_scratch()
--- lua/core/plugins/tmux.lua
+++ lua/core/plugins/tmux.lua
@@ -1,10 +1,21 @@
 return {
   'aserowy/tmux.nvim',
-  event = 'VeryLazy',
+
+  keys = {
+    { '<C-h>',  function() require('tmux').move_left() end,    mode = { 'n', 'v', 'c' } },
+    { '<C-l>',  function() require('tmux').move_right() end,   mode = { 'n', 'v', 'c' } },
+    { '<C-j>',  function() require('tmux').move_bottom() end,  mode = { 'n', 'v', 'c' } },
+    { '<C-k>',  function() require('tmux').move_top() end,     mode = { 'n', 'v', 'c' } },
+  },
+
   opts = {
     navigation = {
       cycle_navigation = false,
-      enable_default_keybindings = true,
+      enable_default_keybindings = false,
+    },
+
+    resize = {
+      enable_default_keybindings = false,
     },
   },
 }
