5cf490dd338f53ef37cfe65109bb7d9f670be9fc
Florian Walter <nairolf.retlaw@gmail.com>
2024-01-13 12:17:45 +0100

    update buffer search keybinding

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -17,7 +17,7 @@ return {
   keys = {
     { '<leader>f', function(...) require('telescope.builtin').find_files(...) end },
     { '<leader>F', function(...) require('telescope.builtin').live_grep(...) end },
-    { '<leader>b', function(...) require('telescope.builtin').buffers(...) end },
+    { '<leader><leader>', function(...) require('telescope.builtin').buffers(...) end },
   },
 
   opts = {
