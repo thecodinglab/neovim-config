a3d225d9f78ce89795f7cafd8f2b45587146ee43
Florian Walter <nairolf.retlaw@gmail.com>
2024-02-01 11:39:30 +0100

    add leap neovim configuration

--- lua/core/plugins/productivity.lua
+++ lua/core/plugins/productivity.lua
@@ -24,6 +24,26 @@ return {
   },
 
   {
+    'ggandor/leap.nvim',
+
+    keys = {
+      { "s", mode = { "n", "x", "o" } },
+      { "S", mode = { "n", "x", "o" } },
+      { "gs", mode = { "n", "x", "o" } },
+    },
+
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
+  },
+
+  {
     'folke/zen-mode.nvim',
 
     cmd = 'ZenMode',
