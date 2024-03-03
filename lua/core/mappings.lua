706931c0b6ff76a7ae836c99cbaacfb97082ad60
Florian Walter <nairolf.retlaw@gmail.com>
2024-03-03 15:46:59 +0100

    add vim diagnostics mappings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -17,6 +17,12 @@ vim.keymap.set('n', '<leader>p', '"+p', opts)
 -- save
 vim.keymap.set('n', '<leader>w', '<cmd>silent write<cr>', opts)
 
+-- diagnostics
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
 -- highlight when yanking
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
