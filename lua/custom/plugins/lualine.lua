bd7074f61cc6214d504f763759bf7d81ca88cf44
Florian Walter <fw@florian-walter.ch>
2024-06-02 11:42:25 +0200

    improve lualine config

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,8 +6,32 @@ return {
 
   opts = {
     options = {
-      icons_enabled = true,
       theme = 'catppuccin',
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+      icons_enabled = true,
     },
+    sections = {
+      lualine_a = {
+        { 'mode', separator = { left = '' }, right_padding = 2 },
+      },
+      lualine_b = { 'filename', 'branch', 'diff' },
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
+      },
+    },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
   },
 }
